aca4899ef3c006f44bec8def0fa897077d11afa0
Miguel Angel <macominom@gmail.com>
2019-03-15 21:52:56 +0100

    Ejercicio

--- competitive-data-science-predict-future-sales/predict_sales.R
+++ competitive-data-science-predict-future-sales/predict_sales.R
@@ -1,8 +1,45 @@
 library(lubridate)
 library(tseries)
+library(dplyr)
 
 setwd("C:/Users/barde/Nextcloud/master/Documentacion/Semana 12 - Series temporales/competitive-data-science-predict-future-sales")
 
 data<-read_csv("sales_train_v2.csv")
 
 
+head(data)
+
+
+
+
+
+data$fechareal <- strptime(data$date, "%d.%m.%Y")
+
+head(data)
+
+
+
+data <- data %>% 
+  mutate(new.date=new.date, week(new.date), month=month(new.date))
+
+
+items <- sort(table(data$item_id), decreasing=T)[1:10]
+
+n.items.per.shop.per.month <- data %>%
+  group_by(month, shop_id, item_id, item_price) %>% 
+             summarise(n = n()) %>% 
+  filter(shop_id %in% seq(1,10)) %>% 
+  filter(item_id %in% names(items)) %>% 
+  summarise(cost = crossprod(item_price, n))
+
+
+
+             
+
+
+dataset <- data %>% 
+  select(month(fechareal), shop_id, item_id, item_price) %>% 
+  group_by(fechareal, shop_id, item_id, item_price)
+
+
+head(dataset)
