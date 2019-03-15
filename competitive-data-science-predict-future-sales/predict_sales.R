81f7c73db0bf67c9d2a81097d7bb943bcda33e8b
Miguel Angel <macominom@gmail.com>
2019-03-15 21:41:49 +0100

    Ejercicio

---
+++ competitive-data-science-predict-future-sales/predict_sales.R
@@ -0,0 +1,8 @@
+library(lubridate)
+library(tseries)
+
+setwd("C:/Users/barde/Nextcloud/master/Documentacion/Semana 12 - Series temporales/competitive-data-science-predict-future-sales")
+
+data<-read_csv("sales_train_v2.csv")
+
+
